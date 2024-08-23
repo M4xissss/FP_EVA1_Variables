8bc29fd55fee1976014036b1873e472feba7b390
DamiannnMxis <L24550379@chihuahua2.tecnm.mx>
2024-08-23 17:57:44 -0600

    Add files via upload

---
+++ EVA_1_Variables/src/eva_1_variables/EVA_1_Variables.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_1_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("HOLA MUNDO"!!);//Cadena de texto 
+    System.out.println(100);//Entero
+    System.out.println(3.1416);//Número con punto flotante
+    System.out.println('c');//Caracter
+    System.out.println(true);//Valores lógicos volátiles (booleanos)
+    System.out.println(
+            
+            false
+            
+            ;//;--> FIN DE INSTRUCCIÓN
+    
+    
+    
+    
+    
+}
