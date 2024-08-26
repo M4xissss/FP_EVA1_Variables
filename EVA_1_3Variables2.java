b4d0cac309cbd09bbecc1c610b624fe9b1420747
DamiannnMxis <L24550379@chihuahua2.tecnm.mx>
2024-08-26 16:38:15 -0700

    Add files via upload

---
+++ EVA_1_3Variables2.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_1_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_1_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("HOLA MUNDO!!");//Cadena de texto
+    System.out.println(100);//Entero
+    System.out.println(3.1416);//Número con punto flotante
+    System.out.println('c');//Caracter
+    System.out.println(true);//Valores lógicos volátiles (booleanos)
+    System.out.println(
+            
+            false)
+            
+            ;//;--> FIN DE INSTRUCCIÓN
+    // ENTEROS:
+    // int 
+    // long
+    // short
+    
+    // NUMEROS CON PARTE DECIMAL:
+    // float
+    // double
+    
+    // CADENAS DE TEXTO:
+    // String
+    
+    // CARACTERES:
+    // char
+    
+    // VALORES LOGICOS:
+    // Boolean
+    
+    // DECLARACIÓN DE VARIABLES:
+    // Sintaxis para declarar variables (REGLAS)
+    // Tipo de dato nombre de la variable 
+    
+    String nombre;
+    String apellido;
+    String curp;
+    double promedio;
+int edad;
+
+    
+    edad = 18; //ASIGNACIÓN: estoy colocando el valor 18
+    //Dentro de la variable edad
+    
+    nombre = "DAMIÁN EDUARDO";
+    apellido = "ISAÍAS DE LUNA";
+    promedio = 8.8;
+        
+    System.out.println(edad);
+    System.out.println(nombre);
+    System.out.println(apellido);
+    System.out.println(promedio);
+    
+
+    }
+        }
+
